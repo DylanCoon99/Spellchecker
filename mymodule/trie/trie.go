38eb997aade7a8cc2bb9667057bc0341098a2937
DylanCoon99 <dylcoon99@gmail.com>
2023-08-15 14:43:47 -0400

    implemented a node

--- mymodule/trie/trie.go
+++ mymodule/trie/trie.go
@@ -12,22 +12,28 @@ import "fmt"
 */
 
 type Node struct {
-	// attributes: value (a letter), and list of children
+	// attributes: val (letter), list of children, and a bit that signifies if at end of a word
+	val byte
+	children []Node
+	end_of_word bool
 }
 
 
 type Trie struct {
 	// k-ary search tree used to store words that are easily searchable
 	// initialize a root node as a list of children nodes
-	
+	root Node
+	max_word_len int
 }
 
-
+/*
 func Constructor() Trie {
 	// instantiates a Trie
 
-}
 
+
+}
+*/
 
 func (this *Trie) Insert(word string) {
 	/*
@@ -46,10 +52,35 @@ func (this *Trie) Search(word string) bool {
 
 	* determines whether or not a string is present in the Trie
 	*/
+	return false
 }
 
 
 
-func PrintHello() {
-	fmt.Println("Hello, World! This is my package speaking!")	
+func PrintDebug() {
+	n1 := Node{
+		val: 'b',
+		children: []Node{},
+		end_of_word: false,
+	}
+
+	n2 := Node{
+		val: 'c',
+		children: []Node{},
+		end_of_word: false,
+	}
+
+	n3 := Node{
+		val: 'd',
+		children: []Node{},
+		end_of_word: false,
+	}
+
+	n4 := Node{
+		val: 'a',
+		children: []Node{n1, n2, n3},
+		end_of_word: false,
+	}
+
+	fmt.Println(n4.val, n4.children, n4.end_of_word)
 }
